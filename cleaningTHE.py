6407bb950fb5be62b3e07e94e9b4e2dab65c504e
1123581321345589144233377610987 <swirlthree@gmail.com>
2025-12-09 00:44:04 -0700

    Flask completely working for random forest

--- cleaningTHE.py
+++ cleaningTHE.py
@@ -1,6 +1,9 @@
 import pandas as np
 import datetime as dt
 file=np.read_csv("THE.csv")
+print(file['value'].describe())
+file=
+"""
 print(file.info())
 print(file.isna().sum())
 print(file.isna().sum()/len(file))
@@ -11,3 +14,4 @@ print(file.info())
 print(file.isna().sum())
 print(file.isna().sum()/len(file))
 file.to_csv('THE.csv')
+"""
